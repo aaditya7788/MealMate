ad9f31bbb985bfbfe82b6036f8f8e5fbebb115af
Aaditya Sahani <aadityasahani78@gmail.com>
2025-05-09 10:47:07 +0530

    Final commit

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,3 @@
-//export const Basic_url = 'http://192.168.0.106:5000';
-//export const Basic_url = 'http://192.168.161.141:5000'; 
-//export const Basic_url = 'https://aaditya78.live';
-export const Basic_url = 'https://mealmate-api-production.up.railway.app';
+
+
+export const Basic_url = 'http://localhost:5000';
